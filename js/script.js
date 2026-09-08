5d5be31242f88d5bc185270486031991ff62f736
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-08 18:05:49 +0100

    Add an ID to each cell of the grid

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector(".container");
+let i;
+let j;
 
-for (let i = 0; i < 16; i++) {
+for (i = 0; i < 16; i++) {
   const column = document.createElement("div");
   column.classList.toggle("column");
   container.appendChild(column);
   
-  for(let j = 0; j < 16; j++) {
+  for(j = 0; j < 16; j++) {
     const row = document.createElement("div");
+    row.setAttribute("id", `div${i}x${j}`);
     row.classList.toggle("row");
     column.appendChild(row);
   }
