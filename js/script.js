c41adcea66dbcf225d6861668d7145814a47466d
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-09 11:56:05 +0100

    Set each cell's background to randomly change

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 const container = document.querySelector(".container");
-let i;
-let j;
 
-for (i = 0; i < 16; i++) {
+for (let i = 0; i < 16; i++) {
   const column = document.createElement("div");
   column.classList.toggle("column");
   container.appendChild(column);
   
-  for(j = 0; j < 16; j++) {
+  for(let j = 0; j < 16; j++) {
     const row = document.createElement("div");
     row.setAttribute("id", `cell${i}x${j}`);
     row.classList.toggle("row");
@@ -15,15 +13,21 @@ for (i = 0; i < 16; i++) {
   }
 }
 
-const cells = document.querySelectorAll("div");
-cells.forEach((cell) => {
-    if (cell.id) {
-        cell.addEventListener("mouseenter", () => {
-            cell.classList.add("class", "mouseenter");
-        });
-
-        cell.addEventListener("mouseleave", () => {
-            cell.classList.add("class", "mouseleave");
-        });
-    }
+container.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = changeBackgroundColor();
+    e.currentTarget.style.backgroundColor = "";
 });
+
+container.addEventListener("mouseout", e => {
+    e.target.style.backgroundColor = changeBackgroundColor();
+    e.currentTarget.style.backgroundColor = "";
+});
+
+function random(number) {
+return Math.floor(Math.random() * (number + 1));
+}
+
+function changeBackgroundColor() {
+const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+return randomColor;
+}
