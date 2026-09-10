0156b2020cd21894c80f43c2c31309e291c34ab8
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-10 14:29:44 +0100

    Refactor the code

--- js/script.js
+++ js/script.js
@@ -1,52 +1,35 @@
-createGrid(16);
+generateGrid(16);
 
-const container = document.querySelector(".container");
-container.addEventListener("mouseover", e => {
-    if (e.target.style.backgroundColor == "") {
-        e.target.style.backgroundColor = changeBackgroundColor();
-        e.target.style.opacity = "10%";
-    }
+const grid = document.querySelector("#grid");
+
+grid.addEventListener("mouseover", e => {
+    const target = e.target.id;
+    const currentTarget = e.currentTarget.id;
+    const targetCurrentBackgroundColor = e.target.style.backgroundColor;
     
-    switch (e.target.style.opacity) {
-        case "0.1":
-            e.target.style.opacity = "20%";
-            break;
-        case "0.2":
-            e.target.style.opacity = "30%";
-            break;
-        case "0.3":
-            e.target.style.opacity = "40%";
-            break;
-        case "0.4":
-            e.target.style.opacity = "50%";
-            break;
-        case "0.5":
-            e.target.style.opacity = "60%";
-            break;
-        case "0.6":
-            e.target.style.opacity = "70%";
-            break;
-        case "0.7":
-            e.target.style.opacity = "80%";
-            break;
-        case "0.8":
-            e.target.style.opacity = "90%";
-            break;
-        case "0.9":
-            e.target.style.opacity = "100%";
-            break;
+    let targetCurrentOpacity = "0";
+    
+    if (target != currentTarget) {
+        targetCurrentOpacity = e.target.style.opacity;
+        targetCurrentOpacity = Number(targetCurrentOpacity);
     }
 
-    e.currentTarget.style.backgroundColor = "";
-    e.currentTarget.style.opacity = "";
+    if ((targetCurrentBackgroundColor == "") && (target != currentTarget)) {
+        e.target.style.backgroundColor = setBackgroundColor();
+        e.target.style.opacity = String(targetCurrentOpacity + 0.1);
+    }
+
+    if ((target != currentTarget) && (targetCurrentOpacity < 1)) {
+        e.target.style.opacity = String(targetCurrentOpacity + 0.1);
+    }
 });
 
-container.addEventListener("mouseout", e => {
-    e.target.style.backgroundColor = changeBackgroundColor();
-    e.currentTarget.style.backgroundColor = "";
+grid.addEventListener("mouseout", e => {
+    e.target.style.backgroundColor = setBackgroundColor();
 });
 
 const button = document.querySelector("button");
+
 button.addEventListener("click", () => {
     let userInput = "";
     
@@ -56,32 +39,36 @@ button.addEventListener("click", () => {
             Number(userInput) > 100 || !Number.isInteger(Number(userInput)));
     
     if (userInput) {
-        while(container.firstChild) {
-            container.removeChild(container.firstChild);
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild);
         }
         
-        createGrid(Number(userInput));
+        generateGrid(Number(userInput));
     }
 });
 
-function random(number) {
-    return Math.floor(Math.random() * (number + 1));
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * (max + 1));
 }
 
-function changeBackgroundColor() {
-    const randomColor = `rgb(${random(255)} ${random(255)} ${random(255)})`;
-    return randomColor;
+function setBackgroundColor() {
+    return `rgb(${getRandomNumber(255)}
+                ${getRandomNumber(255)} 
+                ${getRandomNumber(255)})`;
 }
 
-function createGrid(gridSize) {
-    const container = document.querySelector(".container");
-    for (let i = 0; i < gridSize; i++) {
+function generateGrid(size) {
+    const grid = document.querySelector("#grid");
+    
+    for (let i = 0; i < size; i++) {
         const column = document.createElement("div");
-        column.classList.toggle("column");
-        container.appendChild(column);
         
-        for (let j = 0; j < gridSize; j++) {
+        column.classList.toggle("column");
+        grid.appendChild(column);
+        
+        for (let j = 0; j < size; j++) {
             const row = document.createElement("div");
+            
             row.setAttribute("id", `cell${i}x${j}`);
             row.classList.toggle("row");
             column.appendChild(row);
