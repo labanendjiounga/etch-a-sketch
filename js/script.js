7f63192ff8fa4a946df2e07fece09fe224208c86
Laban <317085441+laban-endji@users.noreply.github.com>
2026-09-11 12:58:38 +0100

    Refactor the code again

--- js/script.js
+++ js/script.js
@@ -1,77 +1,82 @@
 generateGrid(16);
 
+const rainbowCheckbox = document.querySelector("#rainbow");
+let randomColor = "";
+
+rainbowCheckbox.addEventListener("click", () => {
+    randomColor = getRandomColor();
+});
+
 const grid = document.querySelector("#grid");
-
-grid.addEventListener("mouseover", e => {
-    const target = e.target.id;
-    const currentTarget = e.currentTarget.id;
-    const targetCurrentBackgroundColor = e.target.style.backgroundColor;
-    
-    let targetCurrentOpacity = "0";
-    
-    if (target != currentTarget) {
-        targetCurrentOpacity = e.target.style.opacity;
-        targetCurrentOpacity = Number(targetCurrentOpacity);
-    }
-
-    if ((targetCurrentBackgroundColor == "") && (target != currentTarget)) {
-        e.target.style.backgroundColor = setBackgroundColor();
-        e.target.style.opacity = String(targetCurrentOpacity + 0.1);
-    }
-
-    if ((target != currentTarget) && (targetCurrentOpacity < 1)) {
-        e.target.style.opacity = String(targetCurrentOpacity + 0.1);
-    }
-});
-
-grid.addEventListener("mouseout", e => {
-    e.target.style.backgroundColor = setBackgroundColor();
-});
-
 const button = document.querySelector("button");
 
+grid.addEventListener("mouseover", changeBackground);
+grid.addEventListener("mouseout", changeBackground);
 button.addEventListener("click", () => {
-    let userInput = "";
+    const userInput = getUserInput()
     
-    do {
-        userInput = prompt("Enter a whole positive number between 1 and 100");
-    } while (userInput == "" || userInput == "0" || Number(userInput) < 0 ||
-            Number(userInput) > 100 || !Number.isInteger(Number(userInput)));
-    
-    if (userInput) {
-        while (grid.firstChild) {
-            grid.removeChild(grid.firstChild);
-        }
-        
-        generateGrid(Number(userInput));
+    if(userInput) {
+        clearGrid(grid),
+        generateGrid(userInput);
     }
 });
 
-function getRandomNumber(max) {
-    return Math.floor(Math.random() * (max + 1));
+function getRandomColor() {
+    const randomNumber = (max) => Math.floor(Math.random() * (max + 1));
+    return `rgb(${randomNumber(255)}
+                ${randomNumber(255)} 
+                ${randomNumber(255)})`;
 }
 
-function setBackgroundColor() {
-    return `rgb(${getRandomNumber(255)}
-                ${getRandomNumber(255)} 
-                ${getRandomNumber(255)})`;
+function changeBackground(e) {
+    if(e.target) {
+        const currentBackground = e.target.style.backgroundColor;
+        let currentOpacity = Number(e.target.style.opacity);
+        
+        if(currentBackground == "") {
+            (rainbow.checked) ? e.target.style.backgroundColor = getRandomColor() :
+                                e.target.style.backgroundColor = randomColor;
+        }
+        
+        if(currentOpacity < 1) e.target.style.opacity = String(currentOpacity + 0.1);
+    }
 }
 
 function generateGrid(size) {
-    const grid = document.querySelector("#grid");
+    const grid = document.querySelector("#grid");    
     
-    for (let i = 0; i < size; i++) {
+    for(let i = 0; i < size; i++) {
         const column = document.createElement("div");
         
         column.classList.toggle("column");
         grid.appendChild(column);
         
-        for (let j = 0; j < size; j++) {
-            const row = document.createElement("div");
+        for(let j = 0; j < size; j++) {
+            const row = document.createElement("div");           
             
             row.setAttribute("id", `cell${i}x${j}`);
             row.classList.toggle("row");
             column.appendChild(row);
         }
+    }
+}
+
+function getUserInput() {
+    let userInput = "";    
+    
+    do {
+        userInput = prompt("Enter a whole positive number between 16 and 100");
+        if (userInput == null) break;
+    } while(userInput == "" ||
+            Number(userInput) < 16 ||
+            Number(userInput) > 100 ||
+            !Number.isInteger(Number(userInput)));
+    
+    return Number(userInput);
+}
+
+function clearGrid(grid) {
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
 }
